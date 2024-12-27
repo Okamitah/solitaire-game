62e0eb1d707e3faf85b754966c52904d86c3ec0c
okamitah <loneokami03@gmail.com>
2024-12-27 21:15:44 +0100

    Just saving this days progress

--- src/SolitaireGUI.java
+++ src/SolitaireGUI.java
@@ -1,7 +1,50 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SolitaireGUI {
-    JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
+
+
+    public SolitaireGUI() {
+
+        JFrame frame = new JFrame("Solitaire");
+        frame.setSize(1000,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel mainPanel = new JPanel(cardLayout);
+
+        JPanel startMenu = new JPanel();
+        startMenu.setLayout(new BoxLayout(startMenu, BoxLayout.Y_AXIS));
+
+        JLabel menu = new JLabel("Menu");
+        menu.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JButton newGameButton = new JButton("New Game");
+        JButton loadGameButton = new JButton("Load Game");
+        JButton statsButton = new JButton("Statistics");
+        JButton exitButton = new JButton("Exit");
+
+        startMenu.add(menu);
+        startMenu.add(newGameButton);
+        startMenu.add(loadGameButton);
+        startMenu.add(statsButton);
+        startMenu.add(exitButton);
+
+        JPanel gameScreen = new JPanel();
+        gameScreen.setBackground(Color.red);
+        JButton backButton = new JButton("Back to Menu");
+        gameScreen.add(backButton, BorderLayout.SOUTH);
+
+
+        mainPanel.add(startMenu, "Start Menu");
+        mainPanel.add(gameScreen, "Game Screen");
+
+        newGameButton.addActionListener(e -> cardLayout.show(mainPanel, "Game Screen"));
+        exitButton.addActionListener(e -> System.exit(0));
+        backButton.addActionListener(e -> cardLayout.show(mainPanel, "Start Menu"));
+
+        frame.add(mainPanel);
+
+        frame.setVisible(true);
+    }
 
 }
